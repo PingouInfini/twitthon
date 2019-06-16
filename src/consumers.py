28b88791797dc572d20090cc56f136b701ec1c48
pingouinfini <pingouinfini@reachcorp.com>
2019-06-16 19:44:11 +0200

    Modif twitthon pour fonctionnement sans sauvegarde fichier ni thread

--- src/consumers.py
+++ src/consumers.py
@@ -1,61 +1,55 @@
 #!/usr/bin/env python
+import json
 import logging
 import threading
-import json
+
+from kafka import KafkaConsumer
+
 import src.services as services
 import src.variables as variables
-import src.generatormedia as generatormedia
-import src.generatortweet as generatortweet
 
-
-from kafka import KafkaConsumer, KafkaProducer
 
 class Consumer(threading.Thread):
-
     def __init__(self):
         threading.Thread.__init__(self)
-        self.kafka_endpoint=variables.kafka_endpoint
-        self.topic_in=variables.topic_in
-        self.topictweet_out=variables.topictweet_out
-        self.topicmedia_out=variables.topicmedia_out
-        self.outputtweet_user=variables.outputtweet_user
-        self.outputmedia_user=variables.outputmedia_user
-        self.outputtweet_keywords=variables.outputtweet_keywords
-        self.outputmedia_keywords=variables.outputmedia_keywords
-        self.limit=variables.limit
-
+        self.kafka_endpoint = variables.kafka_endpoint
+        self.topic_in = variables.topic_in
+        self.topictweet_out = variables.topictweet_out
+        self.topicmedia_out = variables.topicmedia_out
+        self.topictweet_outdeux=variables.topictweet_outdeux
+        self.outputtweet_user = variables.outputtweet_user
+        self.outputmedia_user = variables.outputmedia_user
+        self.outputtweet_keywords = variables.outputtweet_keywords
+        self.outputmedia_keywords = variables.outputmedia_keywords
+        self.limit = variables.limit
 
     def run(self):
-        logging.info("Lancement du thread pour depiler file kafka "+self.topic_in)
+        logging.info("Lancement du thread pour depiler file kafka " + self.topic_in)
         consumer = KafkaConsumer(bootstrap_servers=self.kafka_endpoint,
                                  value_deserializer=lambda x: json.loads(x.decode('utf-8')),
                                  auto_offset_reset='latest')
 
         consumer.subscribe([self.topic_in])
         for message in consumer:
-            #recuperation des data du json du message
+            # recuperation des data du json du message
             idbio = message.value['idBio']
             nom = message.value['nom']
-            prenom = message.value ['prenom']
-            user =prenom + " "+ nom
+            prenom = message.value['prenom']
+            user = prenom + " " + nom
             accounts = services.get_accounts_from_user(user)
             if len(accounts) > 0:
                 # Get the first account, then get tweets
-                account = accounts[0] # TODO: boucler sur les comptes?
-                #logging.info("### Récupération des " +self.limit+" tweets du compte : "+str(account))
-                services.get_user_tweet(account,self.limit, self.outputtweet_user, self.outputmedia_user)
-                #logging.info("### Récupération des tweets mentionnant le compte : "+str(account))
-                #services.get_tweet_from_keywords(account,self.limit, self.outputtweet_keywords, self.outputmedia_keywords)
-                #logging.info("### Récupération des tweets mentionnant l'utilisateur : "+str(user))
-                #services.get_tweet_from_keywords(user,self.limit, self.outputtweet_keywords, self.outputmedia_keywords)
-                tasks = [
-                    generatormedia.GeneratorMedia(str(account),idbio),
-                    generatortweet.GeneratorTweet(str(account),idbio)
-                ]
-                for t in tasks:
-                    t.start()
+                account = accounts[0]  # TODO: boucler sur les comptes?
 
-                for t in tasks:
-                    t.join()
+                logging.info("### Récupération des " +self.limit+" tweets du compte : "+str(account))
+                services.get_user_tweet_and_push_forward(idbio, account, self.limit, self.kafka_endpoint,
+                                                         self.topictweet_outdeux, self.topicmedia_out)
+
+                # logging.info("### Récupération des tweets mentionnant le compte : "+str(account))
+                # services.get_tweet_from_keywords(account,self.limit, self.outputtweet_keywords, self.outputmedia_keywords)
+
+                # logging.info("### Récupération des tweets mentionnant l'utilisateur : "+str(user))
+                # services.get_tweet_from_keywords(user,self.limit, self.outputtweet_keywords, self.outputmedia_keywords)
+
 
         consumer.close()
--- src/services.py
+++ src/services.py
@@ -1,5 +1,16 @@
+import base64
+import time
+import logging
+from json import dumps
+
+import requests
+from kafka import KafkaProducer
+from tweepy import Cursor
+
 import src.common.twitthon_acquisition
 import src.common.twitthon_authentication
+import src.producers as producers
+
 
 def get_accounts_from_user(user):
     api = src.common.twitthon_authentication.get_api()
@@ -9,20 +20,108 @@ def get_accounts_from_user(user):
     for result in results:
         accounts.append(result._json['screen_name'])
 
-    print("Compte(s) trouve(s) pour '"+str(user)+"' : "+str(accounts))
+    logging.info("Compte(s) trouve(s) pour '" + str(user) + "' : " + str(accounts))
     return accounts
+
+
+def get_user_tweet_and_push_forward(idbio, account, limit, kafka_endpoint, topic_tweet, topic_media):
+    index_m = 0
+    api = src.common.twitthon_authentication.get_api()
+    if account != "":
+        min_id = 9999999999999999999
+        # get first 100 tweets from user
+        nb_tweet_getted = 0
+        total_tweet_getted = 0
+        total_media_getted = 0
+
+        producer = KafkaProducer(bootstrap_servers=[kafka_endpoint],
+                                 value_serializer=lambda x: dumps(x).encode('utf-8'))
+
+        # récupération des 100er tweets...
+        for page in Cursor(api.user_timeline, screen_name=account, count=100).pages(1):
+            for status in page:
+                tweet = status._json
+                nb_tweet_getted += 1
+                print("tweet n°"+str(total_tweet_getted+nb_tweet_getted))
+                if (int(tweet['id_str']) < min_id):
+                    min_id = int(tweet['id_str'])
+
+                # pousser le tweet (+ media) dans les topics:
+                producers.fill_kafka_tweet_idbio(tweet, idbio, producer, topic_tweet)
+                media = extract_media_from_tweet(tweet)
+                if media is not None:
+                    total_media_getted += 1
+                    producers.fill_kafka_picture_idbio(media, idbio, producer, topic_media)
+
+        total_tweet_getted += nb_tweet_getted
+
+        print("recup de "+str(nb_tweet_getted)+"tweets ("+str(total_media_getted)+"medias) / "+str(total_tweet_getted))
+
+        # get next 100 if exists
+        while (nb_tweet_getted > 0):
+
+            if (total_tweet_getted > int(limit)) and int(limit) != 0:
+                logging.info("LIMITE ATTEINTE !!!")
+                break
+
+            nb_tweet_getted = 0
+            for page in Cursor(api.user_timeline, screen_name=account, count=100, max_id=min_id).pages(1):
+                for status in page:
+                    tweet = status._json
+                    if (int(tweet['id_str']) != min_id):
+                        nb_tweet_getted += 1
+                        if (int(tweet['id_str']) < min_id):
+                            min_id = int(tweet['id_str'])
+                        print("tweet n°"+str(total_tweet_getted+nb_tweet_getted))
+                        # pousser le tweet (+ media) dans les topics:
+                        producers.fill_kafka_tweet_idbio(tweet, idbio, producer, topic_tweet)
+                        media = extract_media_from_tweet(tweet)
+                        if media is not None:
+                            total_media_getted += 1
+                            producers.fill_kafka_picture_idbio(media, idbio, producer, topic_media)
+            total_tweet_getted += nb_tweet_getted
+
+            print("recup de "+str(total_tweet_getted+nb_tweet_getted)+"tweets ("+str(total_media_getted)+"medias) / "+str(total_tweet_getted))
+
+
+def extract_media_from_tweet(tweet):
+    try:
+        index = 0
+        for media in tweet['entities']['media']:
+            index += 1
+
+            r = requests.get(media['media_url'], allow_redirects=True)
+
+            if (media['type'] == "photo"):
+                extension = "image/jpg"
+                image = r.content
+            # TODO pour plus tard ... (gestion des medias video et gif)
+            # elif (media['type'] == "video" or media['type'] == "animated_gif"):
+            #     filename += ".mp4"
+            #     io.open(filename, 'wb').write(r.content)
+
+            media = {
+                'name': str(tweet['id']) + "_" + str(index),
+                'extension': extension,
+                'image': base64.b64encode(bytearray(image)).decode('UTF-8')
+            }
+
+            return media
+    except:
+        return None
+
 
 def get_user_tweet(user,
                    limit=500,
-                   outputtweet = "timeline/json",
-                   outputmedia = "timeline/media"):
-
+                   outputtweet="timeline/json",
+                   outputmedia="timeline/media"):
     api = src.common.twitthon_authentication.get_api()
     src.common.twitthon_acquisition.get_user_tweet(api, user, limit, outputtweet, outputmedia)
 
+
 def get_tweet_from_keywords(keywords,
-                            limit = 500,
-                            outputtweet = "keywords/json",
-                            outputmedia = "keywords/media"):
+                            limit=500,
+                            outputtweet="keywords/json",
+                            outputmedia="keywords/media"):
     api = src.common.twitthon_authentication.get_api()
     src.common.twitthon_acquisition.get_tweet_from_keywords(api, keywords, limit, outputtweet, outputmedia)
